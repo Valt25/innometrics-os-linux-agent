27deb795df98fa6d2b0f9993cf00fd36244d6c5f
valeriy <v.gerasimov@innopolis.ru>
2019-04-10 18:33:31 +0300

    Add tray icon with menu

--- frontend/src/main/index.js
+++ frontend/src/main/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-import { app, BrowserWindow } from 'electron'
-
+import { app, Tray, BrowserWindow, ipcMain } from 'electron'
+const path = require('path');
+const Positioner = require('electron-positioner');
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
@@ -10,7 +11,7 @@ if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
-let mainWindow;
+let window, tray;
 const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`;
@@ -19,49 +20,62 @@ function createWindow () {
   /**
    * Initial window options
    */
-  mainWindow = new BrowserWindow({
-    height: 563,
-    useContentSize: true,
-    width: 1000
+
+  window = new BrowserWindow({
+    width: 300,
+    height: 310,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: true,
+    // 'node-integration': false
   });
 
-  mainWindow.loadURL(winURL);
+  window.loadURL(winURL);
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
+  window.on('closedf', () => {
+    window = null
+  });
+
+
+  // Hide the window when it loses focus
+  window.on('blur', () => {
+    if (!window.webContents.isDevToolsOpened()) {
+      window.hide()
+    }
   })
 }
 
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+const toggleWindow = () => {
+  if (window.isVisible()) {
+    window.hide()
+  } else {
+    showWindow()
   }
-});
+};
 
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-});
 
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
-
-/*
-import { autoUpdater } from 'electron-updater'
-
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
+const createTray = () => {
+  let imagePath = path.join(__dirname, 'sunTemplate.png');
+  tray = new Tray(imagePath);
+  tray.on('click', function (event) {
+    toggleWindow()
+  })
+};
 
 app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
- */
+  createTray();
+  createWindow()
+});
+
+const showWindow = () => {
+  let positioner = new Positioner(window);
+  positioner.move('topRight');
+  window.show();
+  window.focus()
+};
+
+ipcMain.on('show-window', () => {
+  showWindow()
+});
